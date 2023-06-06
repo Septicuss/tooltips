d54e153582ad0c4ed5cbb01b3a908d0e90206b4f
Septicuss <40741898+Septicuss@users.noreply.github.com>
2023-06-06 21:04:26 +0300

    fix: Move utility ActionCommand methods

--- src/main/java/fi/septicuss/tooltips/object/preset/actions/command/ActionCommand.java
+++ src/main/java/fi/septicuss/tooltips/object/preset/actions/command/ActionCommand.java
@@ -14,4 +14,19 @@ public interface ActionCommand {
 	public default String getArgument(int index) {
 		return String.valueOf(index);
 	}
+	
+	public default String[] convertArgumentsToArray(Arguments arguments) {
+		String[] args = new String[arguments.size()];
+		for (int i = 1; i <= arguments.size(); i++) {
+			args[i - 1] = arguments.get(String.valueOf(i)).getAsString();
+		}
+		return args;
+	}
+
+	public default String appendFrom(String[] args, int startIndex) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = startIndex; i < args.length; i++)
+			builder.append(args[i] + " ");
+		return builder.toString().strip();
+	}
 }
--- src/main/java/fi/septicuss/tooltips/object/preset/actions/command/impl/vars/VarCommand.java
+++ src/main/java/fi/septicuss/tooltips/object/preset/actions/command/impl/vars/VarCommand.java
@@ -3,25 +3,8 @@ package fi.septicuss.tooltips.object.preset.actions.command.impl.vars;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import fi.septicuss.tooltips.object.preset.condition.argument.Arguments;
-
 public abstract class VarCommand {
 
-	public String[] convertArgumentsToArray(Arguments arguments) {
-		String[] args = new String[arguments.size()];
-		for (int i = 1; i <= arguments.size(); i++) {
-			args[i - 1] = arguments.get(String.valueOf(i)).getAsString();
-		}
-		return args;
-	}
-
-	public String appendFrom(String[] args, int startIndex) {
-		StringBuilder builder = new StringBuilder();
-		for (int i = startIndex; i < args.length; i++)
-			builder.append(args[i] + " ");
-		return builder.toString().strip();
-	}
-	
 	public Player getTarget(String scopeArgument) {
 		return Bukkit.getPlayerExact(scopeArgument);
 	}
