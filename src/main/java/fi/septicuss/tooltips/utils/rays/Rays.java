9e8c29020be6b0e8207a5accc4b1ede0b8607fd0
Septicuss <vbalaban55@gmail.com>
2024-12-10 13:46:59 +0200

    feat: Cached rayTraces

---
+++ src/main/java/fi/septicuss/tooltips/utils/rays/Rays.java
@@ -0,0 +1,110 @@
+package fi.septicuss.tooltips.utils.rays;
+
+import org.bukkit.FluidCollisionMode;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.util.RayTraceResult;
+import org.bukkit.util.Vector;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+
+public class Rays {
+
+    public static final ConcurrentHashMap<Integer, RayTraceResult> CACHED_BLOCK_RAYS = new ConcurrentHashMap<>();
+    public static final ConcurrentHashMap<Integer, RayTraceResult> CACHED_ENTITY_RAYS = new ConcurrentHashMap<>();
+
+    public static BiFunction<Entity, List<EntityType>, Predicate<Entity>> DEFAULT_ENTITY_FILTER = (source, filter) -> {
+        return entity -> {
+            if (entity == null) return false;
+            if (entity.equals(source)) return false;
+            if (filter == null || filter.isEmpty()) return true;
+            return (filter.contains(entity.getType()));
+        };
+    };
+
+    public static RayTraceResult combinedRayTrace(Player player, float distance) {
+        return combinedRayTrace(player, distance, null, null);
+    }
+
+    public static RayTraceResult combinedRayTrace(Player player, float distance, Predicate<Block> blockFilter, Predicate<Entity> entityFilter) {
+        return combinedRayTrace(player.getEyeLocation(), player.getEyeLocation().getDirection(), distance, blockFilter, entityFilter);
+    }
+
+    public static RayTraceResult combinedRayTrace(Location origin, Vector direction, float distance, Predicate<Block> blockFilter, Predicate<Entity> entityFilter) {
+        final int hash = getHash(origin, direction, distance);
+        if (CACHED_BLOCK_RAYS.containsKey(hash)) return CACHED_BLOCK_RAYS.get(hash);
+        if (CACHED_ENTITY_RAYS.containsKey(hash)) return CACHED_ENTITY_RAYS.get(hash);
+
+        final World world = origin.getWorld();
+        if (world == null) return null;
+
+        final RayTraceResult blockRayTrace = Rays.blockRayTrace(origin, direction, distance);
+
+        if (blockRayTrace == null || blockRayTrace.getHitBlock() == null || !blockFilter.test(blockRayTrace.getHitBlock())) {
+            return Rays.entityRayTrace(origin, direction, distance, entityFilter);
+        }
+
+        return blockRayTrace;
+    }
+
+    public static RayTraceResult blockRayTrace(Location origin, Vector direction, float distance) {
+        final int hash = getHash(origin, direction, distance);
+        if (CACHED_BLOCK_RAYS.containsKey(hash)) return CACHED_BLOCK_RAYS.get(hash);
+
+        final World world = origin.getWorld();
+        if (world == null) return null;
+
+        final RayTraceResult result = world.rayTraceBlocks(
+                origin,
+                direction,
+                distance,
+                FluidCollisionMode.NEVER,
+                false
+        );
+
+        Rays.cacheBlockResult(hash, result);
+        return result;
+    }
+
+    public static RayTraceResult entityRayTrace(Location origin, Vector direction, float distance, Predicate<Entity> filter) {
+        final int hash = getHash(origin, direction, distance);
+        if (CACHED_ENTITY_RAYS.containsKey(hash)) return CACHED_ENTITY_RAYS.get(hash);
+
+        final World world = origin.getWorld();
+        if (world == null) return null;
+
+        final RayTraceResult result = world.rayTrace(origin, direction, distance, FluidCollisionMode.NEVER, false, 0, filter);
+
+        Rays.cacheEntityResult(hash, result);
+        return result;
+    }
+
+    public static void clearCache() {
+        CACHED_BLOCK_RAYS.clear();
+        CACHED_ENTITY_RAYS.clear();
+    }
+
+    private static void cacheBlockResult(final int hash, final RayTraceResult result) {
+        if (result == null) return;
+        CACHED_BLOCK_RAYS.put(hash, result);
+    }
+
+    private static void cacheEntityResult(final int hash, final RayTraceResult result) {
+        if (result == null) return;
+        CACHED_ENTITY_RAYS.put(hash, result);
+    }
+
+
+    private static int getHash(final Location origin, final Vector direction, final float distance) {
+        return Objects.hash(origin, direction, distance);
+    }
+
+}
