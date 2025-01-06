371c28b7383f2ff186ffa52df28c0e00ef30ec7f
Septicuss <vbalaban55@gmail.com>
2025-01-06 16:41:50 +0200

    feat: Add context to lookingatcitizen

--- src/main/java/fi/septicuss/tooltips/managers/condition/impl/lookingat/LookingAtCitizen.java
+++ src/main/java/fi/septicuss/tooltips/managers/condition/impl/lookingat/LookingAtCitizen.java
@@ -1,11 +1,13 @@
 package fi.septicuss.tooltips.managers.condition.impl.lookingat;
 
 import fi.septicuss.tooltips.managers.condition.Condition;
+import fi.septicuss.tooltips.managers.condition.Context;
 import fi.septicuss.tooltips.managers.condition.argument.Arguments;
 import fi.septicuss.tooltips.managers.condition.type.MultiString;
 import fi.septicuss.tooltips.utils.Utils;
 import fi.septicuss.tooltips.utils.validation.Validity;
 import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.npc.NPC;
 import net.citizensnpcs.api.npc.NPCRegistry;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
@@ -19,33 +21,57 @@ public class LookingAtCitizen implements Condition {
 	@Override
 	public boolean check(Player player, Arguments args) {
 
+		MultiString ids = null;
+
+		if (args.has(ID))
+			ids = MultiString.of(args.get(ID).getAsString());
+
+		final NPC citizen = this.getCitizen(player, args);
+
+		if (citizen == null) {
+			return false;
+		}
+
+		// Any citizen
+		if (ids == null) {
+			return true;
+		}
+
+		// Specific ID citizen
+		final String id = String.valueOf(citizen.getId());
+		return ids.contains(id);
+	}
+
+	@Override
+	public void writeContext(Player player, Arguments args, Context context) {
+		final NPC citizen = this.getCitizen(player, args);
+
+		if (citizen == null) {
+			return;
+		}
+
+		context.put("citizen.id", citizen.getId());
+	}
+
+	private NPC getCitizen(Player player, Arguments args) {
 		int distance = 3;
 
 		if (args.has(DISTANCE))
 			distance = args.get(DISTANCE).getAsInt();
 
 		var rayTrace = Utils.getRayTraceResult(player, distance);
-		
-		if (rayTrace == null || rayTrace.getHitEntity() == null)
-			return false;
-		
-		Entity entity = rayTrace.getHitEntity();
-		MultiString ids = null;
 
-		if (args.has(ID))
-			ids = MultiString.of(args.get(ID).getAsString());
+		if (rayTrace == null || rayTrace.getHitEntity() == null)
+			return null;
+
+		Entity entity = rayTrace.getHitEntity();
 
 		NPCRegistry registry = CitizensAPI.getNPCRegistry();
 
 		if (!registry.isNPC(entity))
-			return false;
+			return null;
 
-		// Any Citizen
-		if (ids == null)
-			return true;
-
-		String id = String.valueOf(registry.getNPC(entity).getId());
-		return ids.contains(id);
+		return registry.getNPC(entity);
 	}
 
 	@Override
