f918c4d862c52256e8a9103cf544624431cf40f8
Septicuss <vbalaban55@gmail.com>
2025-04-13 01:48:51 +0300

    fix: parse function using wrong context

--- src/main/java/fi/septicuss/tooltips/managers/preset/functions/impl/ParseFunction.java
+++ src/main/java/fi/septicuss/tooltips/managers/preset/functions/impl/ParseFunction.java
@@ -3,7 +3,9 @@ package fi.septicuss.tooltips.managers.preset.functions.impl;
 import fi.septicuss.tooltips.managers.condition.argument.Argument;
 import fi.septicuss.tooltips.managers.preset.functions.Function;
 import fi.septicuss.tooltips.managers.preset.functions.FunctionContext;
+import fi.septicuss.tooltips.utils.Placeholders;
 import fi.septicuss.tooltips.utils.Text;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -16,13 +18,13 @@ public class ParseFunction implements Function {
         if (args.isEmpty()) return "";
         if (args.size() < 2) return "";
 
-        final String targetName = args.get(0).getAsString();
+        final String targetName = args.get(0).process(player, context.context()).getAsString();
         final String string = args.get(1).getAsString();
 
         final Player target = Bukkit.getPlayerExact(targetName);
         if (target == null) return "";
 
-        return Text.processTextWithContext(target, string, context.context());
+        return Placeholders.replacePlaceholders(player, string);
     }
 
 
